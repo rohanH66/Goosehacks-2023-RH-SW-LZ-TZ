7709f3089a60386f832e2a9fa4f09ef81a78f07d
rohanH66 <rohan.hablani@gmail.com>
2023-08-25 17:33:23 -0700

    Update handle_mode_2.py
    Co-Authored-By: svw <107082458+svw2@users.noreply.github.com>
    Co-Authored-By: smartdude2007 <142757647+smartdude2007@users.noreply.github.com>
    Co-Authored-By: Happy-potamus <134818253+Happy-potamus@users.noreply.github.com>

--- handle_mode_2.py
+++ handle_mode_2.py
@@ -1,7 +1,9 @@
 keypress = False
 temp_char = ''
 def handle_keypress(event):
-    
+    global keypress
+    global temp_char
+    temp_char = event.char
     # Print the character associated to the key pressed
     print(temp_char)
     
